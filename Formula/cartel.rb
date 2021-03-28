3dacd66875f4a093dc9c0c718e2c1dbe4513f060
xdrop <xdrop.me@gmail.com>
2021-03-28 18:52:52 +0300

    Bump cartel to 0.7.1

--- Formula/cartel.rb
+++ Formula/cartel.rb
@@ -3,52 +3,31 @@ require_relative "lib/private_strategy"
 class Cartel < Formula
     desc "Local development service orchestrator"
     homepage "https://github.com/xdrop/cartel"
-    url "https://github.com/xdrop/cartel/archive/0.7.0-beta.tar.gz"
-    sha256 "bdb7ce2d80af74a73452c2523d40f7a7bce6fc3d2def7f4915aa2486d1dc05d9"
+    url "https://github.com/xdrop/cartel/archive/0.7.1-beta.tar.gz"
+    sha256 "f9a78686f60688444df961407e2713cf372c163797a3b7fb8d414be3e8e80d50"
 
     depends_on "rust-nightly" => :build
 
     bottle do
-        root_url "https://github.com/xdrop/cartel/releases/download/0.7.0-beta"
+        root_url "https://github.com/xdrop/cartel/releases/download/0.7.1-beta"
         cellar :any_skip_relocation
-        sha256 "2d8cb2d247528035de0d0403b80dfca0d0115d16f150263bbeb8504225ccc33f" => :mojave
-        sha256 "2d8cb2d247528035de0d0403b80dfca0d0115d16f150263bbeb8504225ccc33f" => :catalina
-        sha256 "2d8cb2d247528035de0d0403b80dfca0d0115d16f150263bbeb8504225ccc33f" => :big_sur
+        sha256 "096b036136d61a2c37694cb0e963442ec10903a4a068e539f50a29d61971f88e" => :mojave
+        sha256 "096b036136d61a2c37694cb0e963442ec10903a4a068e539f50a29d61971f88e" => :catalina
+        sha256 "096b036136d61a2c37694cb0e963442ec10903a4a068e539f50a29d61971f88e" => :big_sur
     end
 
     def install
         system "cargo", "build", "--release", "--all"
         bin.install "target/release/client" => "cartel"
         bin.install "target/release/daemon" => "cartel-daemon"
+        prefix.install "launch-daemon.sh"
     end
 
-    plist_options manual: "cartel-daemon"
-
-    def plist
+    def caveats
         <<~EOS
-          <?xml version="1.0" encoding="UTF-8"?>
-          <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-          <plist version="1.0">
-            <dict>
-              <key>Label</key>
-              <string>#{plist_name}</string>
-              <key>RunAtLoad</key>
-              <true/>
-              <key>KeepAlive</key>
-              <true/>
-              <key>ProgramArguments</key>
-              <array>
-                  <string>#{opt_bin}/cartel-daemon</string>
-                  <string>--shell</string>
-                  <string>#{ENV["SHELL"]}</string>
-              </array>
-              <key>WorkingDirectory</key>
-              <string>#{HOMEBREW_PREFIX}</string>
-              <key>SessionCreate</key>
-              <true/>
-            </dict>
-          </plist>
+          Add the following line to your ~/.bash_profile or ~/.zshrc file:
+            [ -f #{opt_prefix}/launch-daemon.sh ] && . #{opt_prefix}/launch-daemon.sh
+          Restart your terminal for the settings to take effect.
         EOS
       end
-
 end
