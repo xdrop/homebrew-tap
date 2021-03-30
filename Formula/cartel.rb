09dd4da2c394231f1a468f557aa78e1a81ebd6af
xdrop <xdrop.me@gmail.com>
2021-03-30 23:07:34 +0300

    Bump cartel to 0.7.3

--- Formula/cartel.rb
+++ Formula/cartel.rb
@@ -3,17 +3,17 @@ require_relative "lib/private_strategy"
 class Cartel < Formula
     desc "Local development service orchestrator"
     homepage "https://github.com/xdrop/cartel"
-    url "https://github.com/xdrop/cartel/archive/0.7.2-beta.tar.gz"
-    sha256 "ec48727ffe5956a925f33f009e06e66d0a2b1e9353749609438faf0599ce053c"
+    url "https://github.com/xdrop/cartel/archive/0.7.3-beta.tar.gz"
+    sha256 "800cb0b7fbee05cdea36a43c0253bf054a3d1d6bba2f22b85089a80b25c0a29b"
 
     depends_on "rust-nightly" => :build
 
     bottle do
-        root_url "https://github.com/xdrop/cartel/releases/download/0.7.2-beta"
+        root_url "https://github.com/xdrop/cartel/releases/download/0.7.3-beta"
         cellar :any_skip_relocation
-        sha256 "719702bf70cee8129f5420a313aee9c98b787729af3f06c656896d58eb04b5fc" => :mojave
-        sha256 "719702bf70cee8129f5420a313aee9c98b787729af3f06c656896d58eb04b5fc" => :catalina
-        sha256 "719702bf70cee8129f5420a313aee9c98b787729af3f06c656896d58eb04b5fc" => :big_sur
+        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :mojave
+        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :catalina
+        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :big_sur
     end
 
     def install
