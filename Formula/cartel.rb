a52a93407276f125af4875f8dfe1fe42eb4574fa
xdrop <xdrop.me@gmail.com>
2021-04-10 13:04:33 +0300

    Bump cartel to 0.7.4
    Also do some linter fixes.

--- Formula/cartel.rb
+++ Formula/cartel.rb
@@ -1,33 +1,32 @@
 require_relative "lib/private_strategy"
 
 class Cartel < Formula
-    desc "Local development service orchestrator"
-    homepage "https://github.com/xdrop/cartel"
-    url "https://github.com/xdrop/cartel/archive/0.7.3-beta.tar.gz"
-    sha256 "800cb0b7fbee05cdea36a43c0253bf054a3d1d6bba2f22b85089a80b25c0a29b"
+  desc "Local development service orchestrator"
+  homepage "https://github.com/xdrop/cartel"
+  url "https://github.com/xdrop/cartel/archive/0.7.4-beta.tar.gz"
+  sha256 "a693a719737f00e4860999ef614cb4c1747730386c6f49f57f28fb9ec71b4c92"
 
-    depends_on "rust-nightly" => :build
+  bottle do
+    root_url "https://github.com/xdrop/cartel/releases/download/0.7.4-beta"
+    sha256 cellar: :any_skip_relocation, mojave:   "95406d4ecb607442f6f89a98f883a7e31e384bf39fb987e94e8ec48bb6d6a748"
+    sha256 cellar: :any_skip_relocation, catalina: "95406d4ecb607442f6f89a98f883a7e31e384bf39fb987e94e8ec48bb6d6a748"
+    sha256 cellar: :any_skip_relocation, big_sur:  "95406d4ecb607442f6f89a98f883a7e31e384bf39fb987e94e8ec48bb6d6a748"
+  end
 
-    bottle do
-        root_url "https://github.com/xdrop/cartel/releases/download/0.7.3-beta"
-        cellar :any_skip_relocation
-        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :mojave
-        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :catalina
-        sha256 "64288582e420369ad06fcbb70d899714254c919bbf9a442b6fa2596d1cc8e45d" => :big_sur
-    end
+  depends_on "rust-nightly" => :build
 
-    def install
-        system "cargo", "build", "--release", "--all"
-        bin.install "target/release/client" => "cartel"
-        bin.install "target/release/daemon" => "cartel-daemon"
-        prefix.install "launch-daemon.sh"
-    end
+  def install
+    system "cargo", "build", "--release", "--all"
+    bin.install "target/release/client" => "cartel"
+    bin.install "target/release/daemon" => "cartel-daemon"
+    prefix.install "launch-daemon.sh"
+  end
 
-    def caveats
-        <<~EOS
-          Add the following line to your ~/.bash_profile or ~/.zshrc file:
-            [ -f #{opt_prefix}/launch-daemon.sh ] && . #{opt_prefix}/launch-daemon.sh
-          Restart your terminal for the settings to take effect.
-        EOS
-      end
+  def caveats
+    <<~EOS
+      Add the following line to your ~/.bash_profile or ~/.zshrc file:
+        [ -f #{opt_prefix}/launch-daemon.sh ] && . #{opt_prefix}/launch-daemon.sh
+      Restart your terminal for the settings to take effect.
+    EOS
+  end
 end
