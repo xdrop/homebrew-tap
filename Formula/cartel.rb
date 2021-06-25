c4474b6e9a0a019598f4c73c26620659d642e69b
xdrop <xdrop.me@gmail.com>
2021-06-25 23:09:21 +0100

    Bump cartel to 0.9.3

--- Formula/cartel.rb
+++ Formula/cartel.rb
@@ -3,14 +3,14 @@ require_relative "lib/private_strategy"
 class Cartel < Formula
   desc "Local development service orchestrator"
   homepage "https://github.com/xdrop/cartel"
-  url "https://github.com/xdrop/cartel/archive/0.9.2-beta.tar.gz"
-  sha256 "9ac4c4f6e2f9fb261e3f6594033d9f25c8964c6dc06025e3130f42dcb9d91977"
+  url "https://github.com/xdrop/cartel/archive/0.9.3-beta.tar.gz"
+  sha256 "054da71652a451656630e4f43026a2d5c8079a3697cedc83d994afd902133f2d"
 
   bottle do
-    root_url "https://github.com/xdrop/cartel/releases/download/0.9.2-beta"
-    sha256 cellar: :any_skip_relocation, mojave:   "cd56c55aca6d2881a8ffc3be4a961a67143f2e7fd6fb64968ab1569f063d5952"
-    sha256 cellar: :any_skip_relocation, catalina: "cd56c55aca6d2881a8ffc3be4a961a67143f2e7fd6fb64968ab1569f063d5952"
-    sha256 cellar: :any_skip_relocation, big_sur:  "cd56c55aca6d2881a8ffc3be4a961a67143f2e7fd6fb64968ab1569f063d5952"
+    root_url "https://github.com/xdrop/cartel/releases/download/0.9.3-beta"
+    sha256 cellar: :any_skip_relocation, mojave:   "dd499df18206325b2c92290390f07ce9ea5abc21247245f6f5962f12b24a09f2"
+    sha256 cellar: :any_skip_relocation, catalina: "dd499df18206325b2c92290390f07ce9ea5abc21247245f6f5962f12b24a09f2"
+    sha256 cellar: :any_skip_relocation, big_sur:  "dd499df18206325b2c92290390f07ce9ea5abc21247245f6f5962f12b24a09f2"
   end
 
   depends_on "rust-nightly" => :build
